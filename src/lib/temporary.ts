8dfbdedf330302e08d43d7aebcee54290d9a73a9
Adrian Wu <eattofueveryday@gmail.com>
2021-02-04 14:03:49 -0500

    m

--- src/lib/temporary.ts
+++ src/lib/temporary.ts
@@ -1,9 +1,13 @@
+export type FlushCallback<T> = (obj: T) => void;
+
 // internal
 class TempStorageEntry<T> {
   timer: ReturnType<typeof setTimeout>;
+  callback: FlushCallback<T>;
   object: T;
   constructor(that: TemporaryStorage<T>, id: string, obj: T, ms: number) {
     this.timer = setTimeout((that2: TemporaryStorage<T>) => {
+      that2.entries[id].callback(that2.entries[id].object);
       delete that2.entries[id];
     }, ms, that);
     this.object = obj;
@@ -11,6 +15,7 @@ class TempStorageEntry<T> {
   refresh(that: TemporaryStorage<T>, id: string, ms: number): void {
     clearTimeout(this.timer);
     this.timer = setTimeout((that2: TemporaryStorage<T>) => {
+      that2.entries[id].callback(that2.entries[id].object);
       delete that2.entries[id];
     }, ms, that);
   }
@@ -28,6 +33,9 @@ export class TemporaryStorage<T> {
   }
   refreshEntry(id: string, ms: number): void {
     this.entries[id].refresh(this, id, ms);
+  }
+  setFlushCallback(id: string, cb: FlushCallback<T>): void {
+    this.entries[id].callback = cb;
   }
   getEntry(id: string): T | null {
     if(this.entries[id]) {
